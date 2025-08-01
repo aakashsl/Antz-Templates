bfe84588181e0cf41c4bb92e4b32db4ed1ffd976
Aakash SL <slaakash06@gmail.com>
2025-08-01 12:40:11 +0530

    Initial commit with Flask app and UI templates
    Add a Flask application with routes for index, toast, popup, animal identification, animal journal, and sample update pages. Include static assets (CSS, images), HTML templates for each page, and a requirements.txt for dependencies. This sets up the project structure and basic UI components for animal lab test management.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+ 
+@app.route('/toast')
+def toast():
+      return render_template('toast.html')
+   
+@app.route('/popup')
+def popup():
+    return render_template('popup.html')
+ 
+@app.route('/AnimalId')
+def AnimalId():
+    return render_template('Animal_Id.html')
+ 
+@app.route('/AnimalJournal')
+def AnimalJournal():
+    return render_template('Animal_Test_Journal.htm')
+ 
+@app.route('/SampleUpdate')
+def sampleUpdate():
+    return render_template('sampleUpdate.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
